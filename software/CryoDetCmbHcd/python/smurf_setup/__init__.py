4be2d6b2e6b041318a3ae1528f340d77a98e454c
cryo <cryo@testbox03.stanford.edu>
2018-04-12 16:27:40 -0700

    getting some empty files in place

---
+++ software/CryoDetCmbHcd/python/smurf_setup/__init__.py
@@ -0,0 +1,4 @@
+import util
+import config
+
+from main import smurf_setup
--- software/CryoDetCmbHcd/python/smurf_setup/main.py
+++ software/CryoDetCmbHcd/python/smurf_setup/main.py
@@ -1,12 +1,4 @@
-# getting the smurfs to smurf, or something like that
-
-init: 
-create instance of experiment from config file
-run setup from bands
-
-
-main 
-step through stages
-
-cleanup
-return to some default state
+def smurf_setup():
+    """                                                                         
+Run complete SMuRF setup."""
+    pass
