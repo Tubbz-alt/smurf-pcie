03f1ad868dd4b28e37be403f5f991073a3d09014
cyndiayu <cyndiayu@stanford.edu>
2018-04-15 17:07:27 -0700

    added basic sysgencryo commands and DAC disable into setup; no ADC/PLL/DAC switching yet

--- software/CryoDetCmbHcd/python/smurf_setup/setup.py
+++ software/CryoDetCmbHcd/python/smurf_setup/setup.py
@@ -1,3 +1,5 @@
+from util.getput import *
+
 def init(smurfCfg):
     """                                                                        
 Initialize SMuRF."""
@@ -7,6 +9,34 @@ Initialize SMuRF."""
     smurfInitCfg=smurfCfg.get('init')
     #for band in smurfInitCfg['bands']:
     #    print(band)
+
+    rootEpics = smurfInitCfg['epics_root']
+    #rootEpics_SysgenCryo = rootEpics + ""
+
+
+    ca_put(rootEpics_SysgenCryo + 'iqSwapIn', smurfInitCfg['iqSwapIn'])
+    ca_put(rootEpics_SysgenCryo + 'iqSwapOut', smurfInitCfg['iqSwapOut'])
+    ca_put(rootEpics_SysgenCryo + 'refPhaseDelay', smurfInitCfg['refPhaseDelay'])
+    ca_put(rootEpics_SysgenCryo + 'refPhaseDelayFine', smurfInitCfg['refPhaseDelayFine'])
+    ca_put(rootEpics_SysgenCryo + 'toneScale', smurfInitCfg['toneScale'])
+    ca_put(rootEpics_SysgenCryo + 'feedbackGain', smurfInitCfg['feedbackGain'])
+    ca_put(rootEpics_SysgenCryo + 'feedbackPolarity', smurfInitCfg['feedbackPolarity'])
+    
+    for band_no in smurfInitCfg['bands']:
+        ca_put(rootEpics_SysgenCryo + 'bandCenterMHz', 4250 + 500 * band_no)
+        # this is currently rewriting
+
+    #for i in range(9):
+        #rootEpics_DAC = rootEpics + ":AMCc:FpgaTopLevel:AppTop:AppTopJesd[0]:JesdTx:" + "dataOutMux[]".format(i)
+        #ca_put(rootEpics_DAC, 'OutputOnes') # this is still hilarious to me
+
+    rootEpics_ADC = rootEpcs + ":AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:CryoAdcMux[0]:"
+    # enable DAC, PLL, map ADC for bands; to do here
+
+    return
+
+
+
 
 def setup(smurfCfg):
     """                                                                        
