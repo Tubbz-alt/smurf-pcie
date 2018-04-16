2df1d979d13c63b829e6bfd772968d7c3b7f96de
cyndiayu <cyndiayu@stanford.edu>
2018-04-16 15:57:22 -0700

    created stage module with generic SmurfTuningStage class

---
+++ software/CryoDetCmbHcd/python/smurf_setup/stage/tuningStage.py
@@ -0,0 +1,20 @@
+class SmurfTuningStage:
+    """initializes, runs, analyzes, and cleans up after a SMuRF tuning stage
+    """
+
+    def __init__(self):
+        # list of required config settings
+        _required=[]
+        _nickname=[]
+
+    def prepare(self):
+        pass
+
+    def run(self):
+        pass
+
+    def analyze(self):
+        pass
+
+    def cleanup(self):
+        pass
