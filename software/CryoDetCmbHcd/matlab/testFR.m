4b3d44e8f7a97c526742eeda94879d2871b82582
cryo <cryo@testbox03.stanford.edu>
2018-04-21 23:33:49 -0700

    Working again with new firmware; now takes single flux ramp frames on all channels.

--- software/CryoDetCmbHcd/matlab/testFR.m
+++ software/CryoDetCmbHcd/matlab/testFR.m
@@ -1,20 +1,46 @@
-function testFR( fr )
-    if nargin < 1
-        % TES?
-        %fr=5426.637;
-        
-        % TES?
-        %fr=5325.069;
-        
-        % Superconducting short?
-        fr=5264.368;
-        
-        %fr=5116.384;
-    end
-    %disp(fr);
-    %disp('Check that fr is right and then press return at prompt to continue ...');
-    %pause;
+function testFR()
+    preTune=false;
+    tuneEach=true;
+    setupEach=false;
+    setupBand=2;
+    relock=true;
+    checklock=false;
+    pauseBeforeEach=false;
+    singleChannel=false;
+    monitorChannel=2;
     
+    root=getSMuRFenv('SMURF_EPICS_ROOT');
+    rootPath=strcat(root,':AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:'); 
+    
+    % keep data organized by date
+    datapath='/data/cpu-b000-hp01/cryo_data/data2/';
+    now=datetime('now')
+    dirdate=datestr(now,'yyyymmdd');
+    datadir=fullfile(datapath,dirdate);
+
+    % if today's date directory doesn't exist yet, make it
+    if not(exist(datadir))
+        disp(['-> creating ' datadir]);
+        mkdir(datadir);
+    end
+
+    filename=num2str(round(posixtime(now)));
+    
+    % save data
+    %% this is redundant, need to make a function
+    % are we in single channel readout mode, and if so, on what channel?
+    singleChannelReadoutOpt2 = lcaGet([rootPath 'singleChannelReadoutOpt2']);
+    readoutChannelSelect = lcaGet([rootPath 'readoutChannelSelect']);
+
+    % add channel suffix for single channel data
+    if singleChannelReadoutOpt2==1
+        filename=[filename '_Ch' num2str(readoutChannelSelect)]
+    end
+    
+    % add .mat suffix
+    filename=fullfile(datadir,[filename '_FvPhi.mat']);
+    disp(['filename=' filename]);
+
     % if user wants plots saved, save them with in a directory with this
     % hardcoded timestamp.
     savePlots=true;
@@ -30,21 +56,19 @@ function testFR( fr )
         end
     end
 
-    root=getSMuRFenv('SMURF_EPICS_ROOT');
-    rootPath=strcat(root,':AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:'); 
-
     legendInfo={};
     counter=1;
     f=[];
     df=[]
     sync=[];
-    v=[]
+    v=[];
+    tunes=[];
 % MD - setup flux ramp before starting
     fluxRampSetup
     
     %% For taking with varying TES voltage
-    varName='VTES';
-    varUnits='V';
+    %varName='VTES';
+    %varUnits='V';
     
     %% Full IV (attempt)
     %normalBias=3.; %V
@@ -55,33 +79,32 @@ function testFR( fr )
     %minV=0.;
     
     %% Short superconducting segment
-    normalBias=7.0; %V
-    normalBiasTime=1; %sec
-    waitBtwIVPoints=0.1;%sec
-    Vstep=0.005; %sec
-    maxV=1.0;
-    minV=0.;
+    %normalBias=7.0; %V
+    %normalBiasTime=1; %sec
+    %waitBtwIVPoints=0.1;%sec
+    %Vstep=0.005; %sec
+    %maxV=1.0;
+    %minV=0.;
     
-    varPoints=fliplr(minV:Vstep:maxV);
+    %varPoints=fliplr(minV:Vstep:maxV);
     %varPoints=[normalBias (normalBias-Vstep) 0];
-    preTune=true;
-    pauseBeforeEach=false;
     
     %% For taking with varying input RF power
-%     varName='Adrive';
-%     varUnits='';
-%     varPoints=[10 10 10 10 10 10 10];
-%     preTune=false;
-%     pauseBeforeEach=false;
+    varName='Adrive';
+    varUnits='';
+    %varPoints=[10 12 11 10 9 8 7 6 5 4];
+    varPoints=[13 12 11 10 9 8];
     %% done varying dependent variable
     
     if preTune
         fluxRampOnOff(0); 
-        chan = setupNotches_umux16_singletone(rootPath,10,[fr]);
-        lcaPut([rootPath,'readoutChannelSelect'], chan(1));
-        lcaPut([rootPath,'singleChannelReadoutOpt2'], 1);
-        fluxRampOnOff(1);
-        pause(1)
+        if singleChannel
+            chan = setupNotches_umux16_singletone(rootPath,10,[fr]);
+            lcaPut([rootPath,'readoutChannelSelect'], chan(1));
+            lcaPut([rootPath,'singleChannelReadoutOpt2'], 1);
+        else 
+            lcaPut([rootPath,'singleChannelReadoutOpt2'], 0);
+        end
     end
     
     % clear figures
@@ -89,12 +112,10 @@ function testFR( fr )
     figure(102); clf;
     
     for var=varPoints
-        if pauseBeforeEach
-            disp(['Press any key on the prompt to take data at ',varName,' = ',num2str(var)]);
-            if var>8
-                pause;
-            end
-        end
+        %if pauseBeforeEach
+        %    disp(['Press any key on the prompt to take data at ',varName,' = ',num2str(var)]);
+        %    pause;
+        %end
         
         %% vary dependent variable
         % what action to take depends on what the user is varying
@@ -117,16 +138,57 @@ function testFR( fr )
         if strcmp(varName,'Adrive')
             fluxRampOnOff(0); 
             pause(0.5);
-            chan = setupNotches_umux16_singletone(rootPath,var,[fr]);
-            lcaPut([rootPath,'readoutChannelSelect'], chan(1));
-            lcaPut([rootPath,'singleChannelReadoutOpt2'], 1);
+            
+            if setupEach
+                setup(setupBand);
+                pause(5);
+            end
+            
+            if tuneEach
+                if singleChannel
+                    chan = setupNotches_umux16_singletone(rootPath,var,[fr]);
+                else
+                    %disp(sprintf('* Press enter to tune at Adrive=%d ...',var));
+                    %pause;
+                    tune_ctime=setupNotches_umux16(rootPath,var,true);
+                    
+                    % keep track of tune at this tone power
+                    [status,cmdout]=system('readlink /data/cpu-b000-hp01/cryo_data/data2/current_eta');
+                    [filepath,name,ext] = fileparts(cmdout) 
+                    [dirpath,dirname] = fileparts(filepath) 
+                    tunes = [tunes ; dirname];
+                    
+                    % for some reason this can improve the number of tones
+                    % which are tracking
+                    if relock
+                        Off;
+                        pause(1);
+                        reLock;
+                    end
+                    
+                    pause(5);
+                end
+            end
+            
+            % change Adrive for all channels
+            pvRoot = [rootPath, 'CryoChannels:'];
+            amplitudeScaleArray=lcaGet( [pvRoot, 'amplitudeScaleArray'] );
+            amplitudeScaleArray(find(amplitudeScaleArray~=0))=var;
+            lcaPut( [pvRoot, 'amplitudeScaleArray'], amplitudeScaleArray );
+            pause(0.5);
+            
             fluxRampOnOff(1); 
             pause(0.5);
             
-            if savePlots
-                figureFilename=fullfile(plotDir,[num2str(plotsCtime),'_res',num2str(round(fr)),'GHz_IQ.png']);
-                saveas(gcf,figureFilename);
+            if checklock
+                pause(5);
+                checkLock;
             end
+            
+            %if savePlots
+            %    figureFilename=fullfile(plotDir,[num2str(plotsCtime),'_res',num2str(round(fr)),'GHz_IQ.png']);
+            %    saveas(gcf,figureFilename);
+            %end
         end
         %% done varying dependent variable
         
@@ -135,24 +197,29 @@ function testFR( fr )
         while count == err_count 
             try
                 % get frame
-                [frameFreq,frameFreqError,frameStrobe] = getFrame( rootPath );
+                clear frameFreq frameFreqError frameStrobe
+                [frameFreq,frameFreqError,frameStrobe] = getFrameByFile( rootPath );
         
                 % plot frame
                 figure(101);
-                plot(frameFreq); hold on;
+                plot(frameFreq(:,monitorChannel)); hold on;
                 legendInfo{counter} = [varName ' = ' num2str(var)];
 
                 % MD - also show frame frequency error - looks to be on a similar scale to frameFreq?           
                 figure(102);
-                plot(frameFreqError); hold on;
+                plot(frameFreqError(:,monitorChannel)); hold on;
                 legendInfo{counter} = [varName ' = ' num2str(var)];
                 counter=counter+1;
         
-                f=horzcat(f,frameFreq);
-                df=horzcat(df,frameFreqError);
-                sync=horzcat(sync,frameStrobe);
-                v=horzcat(v,var);
-                %pause;
+                %f=horzcat(f,frameFreq);
+                %df=horzcat(df,frameFreqError);
+                %sync=horzcat(sync,frameStrobe);
+                
+                f=[f ; frameFreq];
+                df=[df ; frameFreqError];
+                sync=[sync ; frameStrobe];
+                
+                v=[v ; var]
             catch e
                 disp( e.identifier )
                 disp(['ERROR: ', e.message])
@@ -162,13 +229,15 @@ function testFR( fr )
             end
             count = count + 1;
         end
+        %save as we go
+        save(filename,'v','f','df','sync','tunes');
         %pause;
     end
     figure(101);
     legend(legendInfo);
     
     if savePlots
-        figureFilename=fullfile(plotDir,[num2str(plotsCtime),'_res',num2str(round(fr)),'GHz_f.png']);
+        figureFilename=fullfile(plotDir,[num2str(plotsCtime),'_f.png']);
         saveas(gcf,figureFilename);
     end
     
@@ -178,42 +247,13 @@ function testFR( fr )
     title('Feedback error')
     
     if savePlots
-        figureFilename=fullfile(plotDir,[num2str(plotsCtime),'_res',num2str(round(fr)),'GHz_ferr.png']);
+        figureFilename=fullfile(plotDir,[num2str(plotsCtime),'_ferr.png']);
         saveas(gcf,figureFilename);
     end
-    
-    % save data
-    %% this is redundant, need to make a function
-    % are we in single channel readout mode, and if so, on what channel?
-    singleChannelReadoutOpt2 = lcaGet([rootPath 'singleChannelReadoutOpt2']);
-    readoutChannelSelect = lcaGet([rootPath 'readoutChannelSelect']);
-
-    % keep data organized by date
-    datapath='/data/cpu-b000-hp01/cryo_data/data2/';
-    now=datetime('now')
-    dirdate=datestr(now,'yyyymmdd');
-    datadir=fullfile(datapath,dirdate);
-
-    % if today's date directory doesn't exist yet, make it
-    if not(exist(datadir))
-        disp(['-> creating ' datadir]);
-        mkdir(datadir);
-    end
-
-    filename=num2str(round(posixtime(now)));
-    disp(filename);
-
-    % add channel suffix for single channel data
-    if singleChannelReadoutOpt2==1
-        filename=[filename '_Ch' num2str(readoutChannelSelect)]
-    end
-
-    % add .mat suffix
-    filename=fullfile(datadir,[filename '_FvPhi.mat']);
-    disp(['filename=' filename]);
 
     %%
-    save(filename,'v','f','df','sync');
+    save(filename,'v','f','df','sync','tunes');
+    disp(['-> filename=',filename]);
 end
 
 function [frameFreq,frameFreqError,frameStrobe] = getFrame( rootPath )
@@ -231,69 +271,199 @@ function [frameFreq,frameFreqError,frameStrobe] = getFrame( rootPath )
     fs=1/tsamp;
     
     % multiply by 8 to make sure we get the full flux ramp
-    data_length=8*ceil(fs/flux_ramp_rate);
-
+    %data_length=8*ceil(fs/flux_ramp_rate);
+    data_length=2^22;
+    
     time=tsamp*(0:1:data_length-1);
-
+    
     setupDaqMux( 'mitch_epics', 'debug', [], data_length);
     results       = readStreamData( 'mitch_epics', data_length); 
 
-    freqWord      = results(1,:);
-    freqErrorWord = results(2,:);
-    
-    % data is even, strobe is odd, apparently.  
-% % %     freq=freqWord(1:2:end);
-% % %     freqStrobe=freqWord(2:2:end);
+    %%
+    freqWordTemp = results(1,:);
 
-% MD - make data look like uint32 from disk
-   freq1 = double(typecast(int16(freqWord),'uint16')); 
-   freq = freq1(1:2:end) + freq1(2:2:end)*2^16;
-   strobes = floor(freq/2^30);
-   freq = freq - strobes*2^30;
-   fluxRampStrobe = floor(strobes./2);
-    
+    % make unsigned 16
+    idx = find( freqWordTemp < 0 );
+    freqWordTemp(idx) = freqWordTemp(idx) + 2^16;
+    freqWord = freqWordTemp(1:2:end) + freqWordTemp(2:2:end)*2^16;
 
-    neg = find(freq >= 2^23);
-    freq = double(freq);
+    freqErrorWordTemp = results(2,:);
+
+    % make unsigned 16
+    idx = find( freqWordTemp < 0 );
+    freqErrorWordTemp(idx) = freqErrorWordTemp(idx) + 2^16;
+    freqErrorWord = freqErrorWordTemp(1:2:end) + freqErrorWordTemp(2:2:end)*2^16;
+
+    rawData = [freqWord; freqErrorWord];
+    rawData = rawData(:, 3:end)';  % throw away header, transpose to match rawData
+    
+    %%
+    % decode
+    nF = 1; nDF =2;
+    
+    %decode strobes
+    strobes = floor(rawData/2^30);
+    data = rawData - 2^30*strobes;
+    ch0Strobe = mod(strobes, 2) ==1;
+    fluxRampStrobe = floor(strobes/2);
+
+    %decode frequencies
+    ch0idx = find(ch0Strobe(:,1) == 1);
+    Ffirst = ch0idx(1);
+    Flast = ch0idx(length(ch0idx))-1;
+    freqs = data(Ffirst:Flast,1);
+    neg = find(freqs >= 2^23);
+    F = double(freqs);
     if ~isempty(neg)
-         freq(neg) = freq(neg)-2^24;
+        F(neg) = F(neg)-2^24;
     end
-
-     freq = freq * 19.2/2^23;
-
-% % %     freqError=freqErrorWord(1:2:end);
-% % %     freqErrorStrobe=freqErrorWord(2:2:end);
     
-
-    freqError1= double(typecast(int16(freqErrorWord), 'uint16'));
-    freqError = freqError1(1:2:end) + freqError1(2:2:end)*2^16;
-    freqErrorStrobe = floor(freqError./2^30);
-    freqError = freqError - 2^30*freqErrorStrobe;
-    
-    neg = find(freqError >= 2^23);
-%     freqError = double(freqError);
-    if ~isempty(neg)
-         freqError(neg) = freqError(neg)-2^24;
+    if mod(length(F),512)~=0
+       Npoints = length(F)/512;  % bug??
+        F = [];
+    else
+        F = reshape(F,512,[]) * 19.2/2^23;
+        F = F';
     end
-
-     freqError = freqError * 19.2/2^23;
-
-    %decode strobes from freq stream
-% % %     strobes = floor(freqStrobe/2^30);
-% % %     fluxRampStrobe = -floor(strobes/2); % not sure why this has to be negative
-
-    resets = find(fluxRampStrobe >= 0.5);
-
-    % if there aren't 2 resets or more, need longer datasets for each ramp.
-    if length(resets)<2
-        error(['ERROR! Not enough flux ramp resets found in acquisition : length(resets) = ',num2str(length(resets))]);
-    end
-
-    % incorporates strobe at beginning of this flux ramp
-    frameStart=resets(1);
-    frameEnd=resets(2)-1;
-    frameFreq=freq(frameStart:frameEnd);
-    frameFreqError=freqError(frameStart:frameEnd);
-    frameStrobe=fluxRampStrobe(frameStart:frameEnd);
     
+    fluxRampStrobeF = fluxRampStrobe(Ffirst:Flast, nF);
+    
+    %decode frequency errors 
+    % UNTESTED until fixed data stream tested
+    ch0idx = find(ch0Strobe(:,2) == 1);
+    if ~isempty(ch0idx)
+        Dfirst = ch0idx(1);
+        Dlast = ch0idx(length(ch0idx))-1;
+        df = data(Dfirst:Dlast,2);
+        neg = find(df >= 2^23);
+        dF = double(df);
+        if ~isempty(neg)
+            dF(neg) = dF(neg)-2^24;
+        end
+
+        if mod(length(dF),512)~=0
+            Npoints = length(dF)/512;  % bug??
+            dF = [];
+        else
+            dF = reshape(dF,512,[]) * 19.2/2^23;
+            dF = dF';
+        end
+
+    else
+        dF = [];
+    end
+ 
+    
+    
+    fluxRampStrobeDF = fluxRampStrobe(Dfirst:Dlast, nDF);
+    
+%%%
+%%%    freqWord      = results(1,:);
+%%%    freqErrorWord = results(2,:);
+%%%    
+%%%    % data is even, strobe is odd, apparently.  
+%%%% % %     freq=freqWord(1:2:end);
+%%%% % %     freqStrobe=freqWord(2:2:end);
+%%%
+%%%% MD - make data look like uint32 from disk
+%%%   freq1 = double(typecast(int16(freqWord),'uint16')); 
+%%%   freq = freq1(1:2:end) + freq1(2:2:end)*2^16;
+%%%   strobes = floor(freq/2^30);
+%%%   freq = freq - strobes*2^30;
+%%%   fluxRampStrobe = floor(strobes./2);
+%%%    
+%%%    neg = find(freq >= 2^23);
+%%%    freq = double(freq);
+%%%    if ~isempty(neg)
+%%%         freq(neg) = freq(neg)-2^24;
+%%%    end
+%%%
+%%%     freq = freq * 19.2/2^23;
+%%%
+%%%% % %     freqError=freqErrorWord(1:2:end);
+%%%% % %     freqErrorStrobe=freqErrorWord(2:2:end);
+%%%    
+%%%
+%%%    freqError1= double(typecast(int16(freqErrorWord), 'uint16'));
+%%%    freqError = freqError1(1:2:end) + freqError1(2:2:end)*2^16;
+%%%    freqErrorStrobe = floor(freqError./2^30);
+%%%    freqError = freqError - 2^30*freqErrorStrobe;
+%%%    
+%%%    neg = find(freqError >= 2^23);
+%%%%     freqError = double(freqError);
+%%%    if ~isempty(neg)
+%%%         freqError(neg) = freqError(neg)-2^24;
+%%%    end
+%%%
+%%%     freqError = freqError * 19.2/2^23;
+%%%
+%%%    %decode strobes from freq stream
+%%%% % %     strobes = floor(freqStrobe/2^30);
+%%%% % %     fluxRampStrobe = -floor(strobes/2); % not sure why this has to be negative
+%%%
+%%%    resets = find(fluxRampStrobe >= 0.5);
+%%%
+%%%    % if there aren't 2 resets or more, need longer datasets for each ramp.
+%%%    if length(resets)<2
+%%%        error(['ERROR! Not enough flux ramp resets found in acquisition : length(resets) = ',num2str(length(resets))]);
+%%%    end
+%%%
+%%%    % incorporates strobe at beginning of this flux ramp
+%%%    frameStart=resets(1);
+%%%    frameEnd=resets(2)-1;
+%%%    frameFreq=freq(frameStart:frameEnd);
+%%%    frameFreqError=freqError(frameStart:frameEnd);
+%%%    frameStrobe=fluxRampStrobe(frameStart:frameEnd);    
+end
+
+function [frameFreqs,frameFreqErrors,frameStrobes] = getFrameByFile( rootPath )
+
+    global DMBufferSizePV
+
+    C = strsplit(rootPath, ':');
+    root = C{1};
+
+    % close all
+    flux_ramp_rate=1e3; % Hz
+    
+    % why is sample rate 1.2e6, not 2.4e6?
+    tsamp=1/1.2e6; %current sample rate
+    fs=1/tsamp;
+    
+    % multiply by 8 to make sure we get the full flux ramp
+    %data_length=8*ceil(fs/flux_ramp_rate);
+    dataLength=2^21;
+    
+    dfn='/tmp/tmp2.dat';
+    system( ['rm ', dfn] );
+    tic
+    takeDebugData(rootPath,dfn,dataLength);
+    toc
+
+    [f,df,frs]=decodeData(dfn);
+    
+    sync_f = reshape(frs(:,1),512,[]);
+    sync_f = sync_f'; 
+    
+    %% enforce btw 0->1.  Shouldn't have to do this.
+    for col=1:size(sync_f,2)
+        if ~(max(sync_f(:,col))==min(sync_f(:,col)))
+            sync_f(:,col)=(sync_f(:,col)-min(sync_f(:,col)))/(max(sync_f(:,col))-min(sync_f(:,col)));
+        else
+            sync_f(:,col)=0;
+        end
+    end
+    
+    [r,c]=find(sync_f==1);
+    sync_f(r, :) = 1;
+    
+    resets_f = find(sync_f(:,511) == 1);
+    resets_f=resets_f(find(~(diff(resets_f)==1)));
+    
+    frameStart_f=resets_f(1);
+    frameEnd_f=resets_f(2)-1;
+    
+    frameFreqs=f(frameStart_f:frameEnd_f,:);
+    frameFreqErrors=df(frameStart_f:frameEnd_f,:);
+    frameStrobes=sync_f(frameStart_f:frameEnd_f,:);
 end
