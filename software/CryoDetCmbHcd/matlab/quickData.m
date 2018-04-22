5cbfc5fec86bb928314fde99ff0b08e77f4f726b
cryo <cryo@testbox03.stanford.edu>
2018-04-21 23:39:35 -0700

    Added stopwatch, cleaned up variable names

--- software/CryoDetCmbHcd/matlab/quickData.m
+++ software/CryoDetCmbHcd/matlab/quickData.m
@@ -1,6 +1,8 @@
 % take a short dumb dataset
-system('rm /tmp/tmp2.dat');
-takeDebugData(rootPath,'/tmp/tmp2.dat',2^12);
-
 dfn='/tmp/tmp2.dat';
-[f,df,frs]=decodeSingleChannel(dfn);
+system( ['rm ', dfn] );
+tic
+takeDebugData(rootPath,dfn,2^22);
+toc
+
+[f,df,frs]=decodeData(dfn);
