ae333ee2908e98f2bf94385e3b619e3a86e29728
cryo <cryo@testbox03.stanford.edu>
2018-04-21 23:35:46 -0700

    Plot option added as an input, now uses faster array-based etaScan function.

--- software/CryoDetCmbHcd/matlab/etaEstimator.m
+++ software/CryoDetCmbHcd/matlab/etaEstimator.m
@@ -1,4 +1,4 @@
-function [eta, F0, latency, resp, f] = etaEstimator(band, freqs, Adrive, delF)
+function [eta, F0, latency, resp, f] = etaEstimator(band, freqs, Adrive, delF, doPlots)
 %sweep frequencies in a band,
 % aquire complex repsonse vs frequency at dF block where eta is used
 % fit to dF/dS21 to estimate eta
@@ -14,6 +14,11 @@ if nargin <4
     delF = 0.05; %default 50kHz
 end; 
 
+if nargin <5
+    doPlots=true;
+end; 
+
+
 Nread = 1   ;    %normally run with Nread>=4
 
 eta = 0;
@@ -21,49 +26,55 @@ F0 = 0;
 latency = 0;
 
 %dwell = 0.07; [resp, f] = etaScan(band, freqs, Nread, dwell, Adrive);
-dwell = 0.0; [resp, f] = etaScan2(band, freqs, Nread, dwell, Adrive);
-
+dwell = 0.0; [resp, f] = fastEtaScan(band, freqs, Nread, dwell, Adrive);
 
 a = abs(resp); idx = find(a==min(a),1);
 F0 = f(idx) %center frequency in MHz
 left = find(f>F0-delF,1); f(left)
 right = find(f>F0+delF,1); f(right)
 
-figure;
-%assume higher level code has established a figure or we create a new one
-subplot(2,2,1)
-plot(f, a, '.');grid
-hold on
-plot(f(idx), a(idx), 'r*');
-title(['Amplitude Response for Band ' num2str(band)])
-xlabel('Frequency (MHz)')
-ylabel('Response (arbs)')
-ax = axis; xt = ax(1)+0.1*(ax(2)-ax(1)); 
-yt=  ax(3) + 0.1*(ax(4)-ax(3));
-text(xt, yt,['Band Center = ', num2str(band*38.4), ' MHz'])
+if doPlots
+    figure;
+    %assume higher level code has established a figure or we create a new one
+    subplot(2,2,1)
+    plot(f, a, '.');grid
+    hold on
+    plot(f(idx), a(idx), 'r*');
+    title(['Amplitude Response for Band ' num2str(band)])
+    xlabel('Frequency (MHz)')
+    ylabel('Response (arbs)')
+    ax = axis; xt = ax(1)+0.1*(ax(2)-ax(1)); 
+    yt=  ax(3) + 0.1*(ax(4)-ax(3));
+    text(xt, yt,['Band Center = ', num2str(band*38.4), ' MHz'])
  
-netPhase = unwrap(angle(resp));
-%figure(2); 
-subplot(2,2,2), plot(f, netPhase, '.');grid
-title(['Phase Response for Band ' num2str(band)])
-xlabel('Frequency (MHz)')
-ylabel('Phase')
-Nf = length(f);
-latency = (netPhase(Nf)-netPhase(1))/(f(Nf)-f(1))/2/pi
-hold on
-plot(f(left), netPhase(left), 'gx')
-plot(f(right), netPhase(right), 'g+')
+    netPhase = unwrap(angle(resp));
+    %figure(2); 
+    subplot(2,2,2), plot(f, netPhase, '.');grid
+    title(['Phase Response for Band ' num2str(band)])
+    xlabel('Frequency (MHz)')
+    ylabel('Phase')
+    Nf = length(f);
+    latency = (netPhase(Nf)-netPhase(1))/(f(Nf)-f(1))/2/pi
+    hold on
+    plot(f(left), netPhase(left), 'gx')
+    plot(f(right), netPhase(right), 'g+')
 
-%complex Response Plot
-%figure(3)
-subplot(2,2,3), plot(resp, '.');grid, hold on
-plot(resp(idx),'r*')
-plot(resp(left), 'gx')
-plot(resp(right), 'g+')
-title(['Complex Response for Band ' num2str(band)])
+    p = polyfit([f(left), f(right)], [netPhase(left), netPhase(right)], 1);
+        plot(f, polyval(p,f), 'k--')
+    axis([f(1), f(end), min(netPhase) - 0.2, max(netPhase) + 0.2])
 
-hold off
-axis([-0.5 0.5 -0.5 0.5])
+    %complex Response Plot
+    %figure(3)
+    subplot(2,2,3), plot(resp, '.');grid, hold on
+    plot(resp(idx),'r*')
+    plot(resp(left), 'gx')
+    plot(resp(right), 'g+')
+    title(['Complex Response for Band ' num2str(band)])
+
+    hold off
+    pbaspect([1 1 1])
+    %axis([-0.25 0.25 -0.25 0.25])
+end
 
 %estimate eta
 eta = (f(right)-f(left))/(resp(right)-resp(left))
@@ -72,13 +83,16 @@ etaPhase = angle(eta);
 etaPhaseDeg = angle(eta)*180/pi
 etaScaled = etaMag/19.2
 
-%figure(4)
-subplot(2,2,4), plot(resp*eta, '.'), grid, hold on
-plot(eta*resp(idx),'r*')
-plot(eta*resp(right), 'g+')
-plot(eta*resp(left), 'gx')
-
-axis([-0.5 0.5 -0.5 0.5])
+if doPlots
+    %figure(4)
+    subplot(2,2,4), plot(resp*eta, '.'), grid, hold on
+    plot(eta*resp(idx),'r*')
+    plot(eta*resp(right), 'g+')
+    plot(eta*resp(left), 'gx')
+    
+    pbaspect([1 1 1])
+    %axis([-0.25 0.25 -0.25 0.25])
+end
 
 end
 % end of function etaEstimator
@@ -194,123 +208,3 @@ resp = respI + 1i*respQ;    %form complex response
 Adrive = 0; % turn channel OFF
 configCryoChannel( rootPath, subchan, freqs(j), Adrive, 0, 0, 0 ) ;
 end
-
-
-%_________________________________________________________________________
-% subfunction etaScan2
-
-function [resp, f] = etaScan2(band, freqs, Nread, dwell, Adrive)
-% Faster version of etaScan based on PVs Mitch added to pyrogue
-% Can't implement until we've worked some bugs out of new pyrogue...
-% SWH 21Mar2018
-
-rootPath = 'mitch_epics:AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:CryoChannels:';
-
-if nargin <3 
-    Nread = 2; % default number of reads per frequnecy setting
-end; 
-
-if nargin <4 
-    dwell = 0.001; %dwell time default is 1 ms
-end; 
-
-f=repelem(freqs,Nread);
-%
-%if length(repfreqs)>1
-%    error(sprintf('!!! Error in etaScan2: asked for %d freqs in scan; must be less than 1000',length(repfreqs)))
-%end
-%%
-
-subchan = 16*band;
-lcaPut([rootPath,'etaScanFreqs'],f);
-lcaPut([rootPath,'etaScanAmplitude'],Adrive);
-lcaPut([rootPath,'etaScanChannel'],subchan);
-lcaPut([rootPath,'etaScanDwell'],0);
-
-
-%% SHOULD MONITOR RESULTS INSTEAD OF ETASCANINPROGRESS PV TO DETERMINE WHEN ETASCAN COMPLETES
-%% Monitor etaScanInProgress register to figure out when scan on this channel is complete.
-%% This is not the best way of doing this, probably; Mitch suggests monitoring the results
-%% instead of this etaScanInProgressRegister.  For ex. the below will fail if the monitor
-%% doesn't overlap with the duration of the scan (since it requires that at some point
-%% during monitoring, etaScanInProgress was =1.
-etaScanInProgress=false;
-etaScanProgressPV='mitch_epics:AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:CryoChannels:etaScanInProgress';
-% Set a monitor
-lcaSetMonitor(etaScanProgressPV);
-val0=lcaGet(etaScanProgressPV);
-
-% run the etaScan
-lcaPutNoWait([rootPath,'runEtaScan'],1);
-
-disp('-> Waiting for etaScan to start...');
-
-% check if etaScan has already started or not
-if val0==1
-    etaScanInProgress=true;
-    disp('-> etaScan started.');
-end 
-while true
-    try lcaNewMonitorWait(etaScanProgressPV)
-        val=lcaGet(etaScanProgressPV);
-    catch errs
-        if errs.identifier == 'labca:timedOut' 
-            lcaPutNoWait([rootPath,'runEtaScan'],1);
-            disp(' ')
-            disp(' ')
-            disp(' ')
-            disp(' ')
-            disp('rearm')
-            disp(' ')
-            disp(' ')
-            disp(' ')
-            disp(' ')
-        else
-            
-    %         errs = lcaLastError();
-            fprintf(1,'The identifier was:\n%s',errs.identifier);
-            fprintf(1,'There was an error! The message was:\n%s',errs.message);
-            handleErrors(errs);
-        end
-    end
-    % need to make sure etaScan was actually started before 
-    % looking for its end
-	if val==1
-        etaScanInProgress=true;
-        disp('-> etaScan started.');
-    end
-    % scan is complete if the monitor saw that it was running
-    % (etaScanInProgress=1 while monitor was running) and
-    % etaScanInProgress has subsequently been toggled low.
-    if val==0 && etaScanInProgress
-        disp('-> etaScan completed.');
-        break;
-    end
-end
-
-% Clear the monitor
-lcaClear(etaScanProgressPV);
-%% Done monitoring etaScanInProgress to determine when scan on this channel is complete
-
-I=lcaGet([rootPath,'etaScanResultsReal'],length(f));
-Q=lcaGet([rootPath,'etaScanResultsImag'],length(f));
-
-if I >= 2^23
-   I  = I-2^24;   %treat as signed 24 bit
-end
-
-if Q >= 2^23
-   Q  = Q-2^24;   %treat as signed 24 bit
-end
-
-I = I/2^23;
-Q = Q/2^23;
-
-resp = I + 1i*Q;    %form complex response
-
-Adrive = 0; % turn channel OFF
-rootPath = 'mitch_epics:AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:';
-configCryoChannel( rootPath, subchan, freqs(ceil(end/2)), Adrive, 0, 0, 0 ) ;
-
-end
-% end of function etaScan2
