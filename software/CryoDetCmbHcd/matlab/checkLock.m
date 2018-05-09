5c0440096b144d72860a26bf30065ab6f6933bc9
cryo <cryo@testbox03.stanford.edu>
2018-05-09 14:41:03 -0700

    getData is broken for some reason, switching for now to using takeDebugData in the checkLock script

--- software/CryoDetCmbHcd/matlab/checkLock.m
+++ software/CryoDetCmbHcd/matlab/checkLock.m
@@ -12,7 +12,16 @@ function lockedChannels=checkLock(chans)
         chans=chans+1;
     end
 
-    [f,df,frs]=getData(rootPath,2^18);
+    % take a short dumb dataset
+    dfn='/tmp/tmp2.dat';
+    system( ['rm ', dfn] );
+
+    takeDebugData(rootPath,dfn,2^20);
+
+    %[f, df, frs] = decodeSingleChannel(dfn, 0);
+    [f,df,frs]=decodeData(dfn);
+    
+    %[f,df,frs]=getData(rootPath,2^18);
     % only loop over defined channels
     numChannels=0
     numKilled=0
