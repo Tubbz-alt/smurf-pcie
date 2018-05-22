3487f8b02097973a30c4efcde777e05831b9f1a4
cryo <cryo@testbox03.stanford.edu>
2018-05-22 11:11:09 -0700

    whichOn now figures out which channels are on by polling amplitude and feedbackEnable arrays; much faster

--- software/CryoDetCmbHcd/matlab/whichOn.m
+++ software/CryoDetCmbHcd/matlab/whichOn.m
@@ -1,13 +1,18 @@
 %script whichOn
 %turn off all channels
 %Note this resets all frequencies and eta values
+function whichOn=checkLock(chans)
+    rootPath = [getSMuRFenv('SMURF_EPICS_ROOT'),':AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:'];
+    pvRoot = [rootPath, 'CryoChannels:'];
 
-rootPath = [getSMuRFenv('SMURF_EPICS_ROOT'),':AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:'];
+    amplitudeScaleArray = lcaGet( [pvRoot, 'amplitudeScaleArray'] );
+    feedbackEnableArray = lcaGet( [pvRoot, 'feedbackEnableArray'] );
 
+    whichOn=(find(amplitudeScaleArray)-1);
 
-for n=0:511
-    [band, Freq, ampl] = getChannel(n);
-    if ampl > 0
-        display(['found channel ' num2str(n) ' set to amplitude ' num2str( ampl)])
-    end
-end
+    %for n=0:511
+    %    [band, Freq, ampl] = getChannel(n);
+    %    if ampl > 0
+    %        display(['found channel ' num2str(n) ' set to amplitude ' num2str( ampl)])
+    %    end
+    %end
