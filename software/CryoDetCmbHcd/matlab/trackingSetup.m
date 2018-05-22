e05c574b7bc8419fc2d36ff9e7d243dfb680f8fb
cryo <cryo@testbox03.stanford.edu>
2018-05-22 11:21:00 -0700

    Have been doing lots of testing with these flux ramp/tracking algo settings; phi0rate is ~13kHz.

--- software/CryoDetCmbHcd/matlab/trackingSetup.m
+++ software/CryoDetCmbHcd/matlab/trackingSetup.m
@@ -1,22 +1,43 @@
-% rough calibration of phi0 to fraction of the flux ramp fullscale
-fluxRampFullScale_to_Phi0=4.540/0.7; % measured at 1kHz
-resetRatekHz=1.000;
-fractionFullScaleDesired=0.7;
+resetRatekHz=4.0;
+fractionFullScaleDesired=0.50;
 
-lmsDelay   = 5;    % nominally match refPhaseDelay
-lmsGain    = 5;    % incrases by power of 2, can also use etaMag to fine tune
+% rough calibration of phi0 to fraction of the flux ramp fullscale
+%% 14dB input atten, directly from RTM monitor port
+fluxRampFullScale_to_Phi0=4.565/0.7; % measured at 1kHz
+%% 9dB input atten, RTM monitor port -> NIST isolation amp -> fridge
+%fluxRampFullScale_to_Phi0=4.410/0.7; % measured at 1kHz
+
+lmsDelay   = 6;    % nominally match refPhaseDelay
+lmsGain    = 7;    % incrases by power of 2, can also use etaMag to fine tune
 lmsEnable1 = 1;    % 1st harmonic tracking
 lmsEnable2 = 1;    % 2nd harmonic tracking
 lmsEnable3 = 1;    % 3rd harmonic tracking
 lmsRstDly  = 31;   % disable error term for 31 2.4MHz ticks after reset
 lmsFreqHz  = fluxRampFullScale_to_Phi0*fractionFullScaleDesired*(resetRatekHz*10^3); % fundamental tracking frequency guess
-lmsDly2    = 50;   % delay DDS counter resets, 307.2MHz ticks
+lmsDly2    = 100;   % delay DDS counter resets, 307.2MHz ticks
 lmsDlyFine = 0;
-
 iqStreamEnable = 0; % stream IQ data from tracking loop
                     % ***currently channel not matching f df readout***
        
 optimize=false;
+lmsFreqHz=13052.8571;
+%lmsFreqHz=lmsFreqHz+40;
+%lmsFreqHz=4846.0714;
+
+%if resetRatekHz>=5.000
+%    lmsDelay   = 6;
+%    lmsGain    = 4; 
+%end
+%
+%if resetRatekHz==5.000
+%    lmsFreqHz = 22825;
+%end
+%if resetRatekHz==6.000
+%    lmsFreqHz = 27470;
+%end
+%if resetRatekHz==3.000
+%    lmsFreqHz = 13715;
+%end
                     
 if optimize
     optFreqRange=(lmsFreqHz-40):5:(lmsFreqHz+40)
