fabc0df3422bd042fb20d0d1e90c53d458238319
cryo <cryo@testbox03.stanford.edu>
2018-05-22 11:12:52 -0700

    Made quickData a function instead of just a script; keeps the f/df/frs arrays it returns from overwriting those variables since we use them in a lot of scripts.

--- software/CryoDetCmbHcd/matlab/quickData.m
+++ software/CryoDetCmbHcd/matlab/quickData.m
@@ -1,8 +1,10 @@
-% take a short dumb dataset
-dfn='/tmp/tmp2.dat';
-system( ['rm ', dfn] );
+function [f,df,frs]=quickData()
+    % take a short dumb dataset
+    dfn='/tmp/tmp2.dat';
+    system( ['rm ', dfn] );
 
-takeDebugData(rootPath,dfn,2^20);
-
-%[f, df, frs] = decodeSingleChannel(dfn, 0);
-[f,df,frs]=decodeData(dfn);
+    rootPath = [getSMuRFenv('SMURF_EPICS_ROOT'),':AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:'];
+    takeDebugData(rootPath,dfn,2^20);
+    
+    %[f, df, frs] = decodeSingleChannel(dfn, 0);
+    [f,df,frs]=decodeData(dfn);
