f45a587a64f0aa13e33b375b797d1a6986522f47
cryo <cryo@testbox03.stanford.edu>
2018-05-24 16:11:09 -0700

    Now takes optional argument; # of points

--- software/CryoDetCmbHcd/matlab/quickData.m
+++ software/CryoDetCmbHcd/matlab/quickData.m
@@ -1,10 +1,15 @@
-function [f,df,frs]=quickData()
+function [f,df,frs]=quickData(Npts)
+
+    if nargin<1
+        Npts=2^20
+    end
+
     % take a short dumb dataset
     dfn='/tmp/tmp2.dat';
     system( ['rm ', dfn] );
 
     rootPath = [getSMuRFenv('SMURF_EPICS_ROOT'),':AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:'];
-    takeDebugData(rootPath,dfn,2^20);
+    takeDebugData(rootPath,dfn,Npts);
     
     %[f, df, frs] = decodeSingleChannel(dfn, 0);
     [f,df,frs]=decodeData(dfn);
