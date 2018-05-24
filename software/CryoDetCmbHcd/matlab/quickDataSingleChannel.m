46f8a893586173898e3164e778b3df8a415cdfe8
cryo <cryo@testbox03.stanford.edu>
2018-05-24 16:15:39 -0700

    swapFdF not needed anymore, also arguments have changed.

--- software/CryoDetCmbHcd/matlab/quickDataSingleChannel.m
+++ software/CryoDetCmbHcd/matlab/quickDataSingleChannel.m
@@ -2,7 +2,8 @@
 dfn='/tmp/tmp2.dat';
 system( ['rm ', dfn] );
 
+rootPath = [getSMuRFenv('SMURF_EPICS_ROOT'),':AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:'];
 takeDebugData(rootPath,dfn,2^16);
 
-[f, df, frs] = decodeSingleChannel(dfn, 0);
+[f, df, frs] = decodeSingleChannel(dfn);
 %[f,df,frs]=decodeData(dfn);
