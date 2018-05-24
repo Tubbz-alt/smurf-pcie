9d68799456d9ff19822ef281c6355b1044e5664f
cryo <cryo@testbox03.stanford.edu>
2018-05-24 16:10:49 -0700

    Smarter, know looks up etaScan automatically, can figure out what channels were programmed better, etc.

--- software/CryoDetCmbHcd/matlab/plotChannels.m
+++ software/CryoDetCmbHcd/matlab/plotChannels.m
@@ -1,56 +1,90 @@
-%plotChannels
-%dfn='/home/common/data/cpu-b000-hp01/cryo_data/data2/20180316/1521234479.dat';
-%% 100Hz, 0.13Vpp flux ramp
-dfn='/home/common/data/cpu-b000-hp01/cryo_data/data2/20180316/1521235661.dat';
-[f,df,frs]=decodeData(dfn);
-%% 0VDC flux ramp fixed bias
-dfn2='/home/common/data/cpu-b000-hp01/cryo_data/data2/20180316/1521235998.dat';
-[f2,df2,frs2]=decodeData(dfn2);
+close all;
+clear;
 
-load('/data/cpu-b000-hp01/cryo_data/data2/20180316/1521220641/1521220641_etaOut.mat');
+baseNumber=0;
+baseRootPath = [getSMuRFenv('SMURF_EPICS_ROOT'),sprintf(':AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[%d]:',baseNumber)]
 
-%dfn='/home/common/data/cpu-b000-hp01/cryo_data/data2/20180316/1521235504_Ch18.dat';
-%[f,df,frs]=decodeSingleChannel(dfn);
+pre_singleChannelReadout=lcaGet( [baseRootPath, 'singleChannelReadout'])
+pre_singleChannelReadoutOpt2=lcaGet( [baseRootPath, 'singleChannelReadoutOpt2'])
+pre_iqStreamEnable=lcaGet( [baseRootPath, 'iqStreamEnable'])
+pre_decimation=lcaGet( [baseRootPath, 'decimation'])
+pre_filterAlpha=lcaGet( [baseRootPath, 'filterAlpha'])
+
+lcaPut( [baseRootPath, 'singleChannelReadout'], 0)
+lcaPut( [baseRootPath, 'singleChannelReadoutOpt2'], 0)
+lcaPut( [baseRootPath, 'iqStreamEnable'], 0)
+lcaPut( [baseRootPath, 'decimation'], 0)
+lcaPut( [baseRootPath, 'filterAlpha'], 16384);
+
+[f,df,frs]=quickData(2^18);
+
+%% return to pre-script readout configuration
+lcaPut( [baseRootPath, 'singleChannelReadout'], pre_singleChannelReadout)
+lcaPut( [baseRootPath, 'singleChannelReadoutOpt2'], pre_singleChannelReadoutOpt2)
+lcaPut( [baseRootPath, 'iqStreamEnable'], pre_iqStreamEnable)
+lcaPut( [baseRootPath, 'decimation'],pre_decimation)
+lcaPut( [baseRootPath, 'filterAlpha'], pre_filterAlpha);
+
+% keep track of tune at this tone power                     
+[status,cmdout]=system('readlink /data/cpu-b000-hp01/cryo_data/data2/current_eta');            
+etaFilePath=strtrim(cmdout);
+eta=load(etaFilePath);
+
+etaCfgFilePath=strrep(etaFilePath,'_etaOut.mat','.mat');
+eta_cfg=load(etaCfgFilePath);
+
+Nchans=(eta_cfg.numberChannels/eta_cfg.numberSubBands);
+
+%% which bands have channels with nonzero data?
+% which channels have measured eta parameters?
+etaOut=eta.etaOut;
+etaChan={etaOut.chan};
+chans=etaChan(find(~cellfun(@isempty,etaChan)));
+chans=cell2mat(chans);
+bands=[];
+for chan=chans
+    bands=[bands,getChannelSubBand(baseNumber,chan)];
+end
+bands=sort(unique(bands));
 
 %how many lines shall we plot?
-for band=31
-    Nchans = 8;
+for band=bands
     fig=figure;
-    subplot(8,1,1)
+    subplot(Nchans,1,1)
     nplot=1;
     title(sprintf('band %d',band));
-    for chan = (1+16*band):(Nchans+16*band)
-        spanf=(max(f(:,chan))-min(f(:,chan)));
-        midf=min(f(:,chan))+spanf/2.;
-        subplot(8, 1, nplot)
-        plot(1e3*(f(:,chan)-midf));%, grid, title(['Channel ' num2str(chan)])
-        hold on;
-        plot(1e3*(f2(:,chan)-midf));%, grid, title(['Channel ' num2str(chan)])
+    
+    bandchans=getChannelsInSubBand(baseNumber,band);
+    for chan = bandchans
+        spanf=(max(f(:,chan+1))-min(f(:,chan+1)));
+        midf=min(f(:,chan+1))+spanf/2.;
+        subplot(Nchans, 1, nplot)
+        plot(1e3*(f(:,chan+1)-midf));%, grid, title(['Channel ' num2str(chan)])
         
-        if chan==(1+16*band)
+        if chan==bandchans(1)
             text(0.5,2.0,sprintf('band %d',band),'Units','normalized','FontSize',18,'HorizontalAlignment','center');
         end
         
-        if ~(chan==1)
-            if ~isempty(etaOut(chan-1).eta)
-                title(sprintf('band%d, ch%d - %0.3f GHz',band,chan-1,etaOut(chan-1).res));
-            else
-                title(sprintf('band%d, ch%d - not programmed',band,chan-1));
-            end
+        if any(chans(:) == chan);
+            %title(sprintf('band%d, ch%d - %0.3f GHz',band,chan,etaOut(chan-1).res));
+            title(sprintf('band%d, ch%d',band,chan));
         else
-            title(sprintf('band%d, ch%d etaScan parameters accidentally not programmed due to indexing error',band,chan-1));
+            title(sprintf('band%d, ch%d - not programmed',band,chan));
         end
+
         text(0.6,0.15,sprintf('Foff = %0.3f MHz, dFr = %0.0f kHz',midf,spanf*1000),'Units','normalized','FontSize',8);
         ylabel('F (kHz)');
         nplot = nplot+1;
-        if chan<(Nchans+16*band)
+        
+        if ~(chan==bandchans(end))
             set(gca,'XTick',[]);
         else
             xlabel('Sample number');
         end
         ylim([-150,150]);
-        set(gcf,'Units','Normalized','OuterPosition',[0.04,0.04,0.6,0.96]);
+        %set(gcf,'Units','Normalized','OuterPosition',[0.04,0.04,0.6,0.96]);
     end
-    figureFileName=sprintf('1521220641_8chan_band%d.png',band);
+    ctime=ctimeForFile();
+    figureFileName=sprintf('%d_band%d.png',ctime,band);
     saveas(gcf,figureFileName);
 end
