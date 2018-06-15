02d86778e249b45702bd388b3dda63082560a985
cryo <cryo@testbox03.stanford.edu>
2018-06-14 21:44:01 -0700

    whichOn now checks by band, with band # as input.

--- software/CryoDetCmbHcd/matlab/whichOn.m
+++ software/CryoDetCmbHcd/matlab/whichOn.m
@@ -1,14 +1,14 @@
 %script whichOn
 %turn off all channels
 %Note this resets all frequencies and eta values
-function whichOn=checkLock(chans)
-    rootPath = [getSMuRFenv('SMURF_EPICS_ROOT'),':AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:'];
+function on=whichOn(band)
+    rootPath = [getSMuRFenv('SMURF_EPICS_ROOT'),sprintf(':AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[%d]:',band)];
     pvRoot = [rootPath, 'CryoChannels:'];
 
     amplitudeScaleArray = lcaGet( [pvRoot, 'amplitudeScaleArray'] );
     feedbackEnableArray = lcaGet( [pvRoot, 'feedbackEnableArray'] );
 
-    whichOn=(find(amplitudeScaleArray)-1);
+    on=(find(amplitudeScaleArray)-1);
 
     %for n=0:511
     %    [band, Freq, ampl] = getChannel(n);
