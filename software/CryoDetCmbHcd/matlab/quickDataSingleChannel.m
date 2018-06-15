b6cd0333c100bba9582056f4c055c9048789c7f3
cryo <cryo@testbox03.stanford.edu>
2018-06-14 21:57:28 -0700

    quickDataSingleChannel is now a function, takes band no. as input.

--- software/CryoDetCmbHcd/matlab/quickDataSingleChannel.m
+++ software/CryoDetCmbHcd/matlab/quickDataSingleChannel.m
@@ -1,9 +1,10 @@
-% take a short dumb dataset
-dfn='/tmp/tmp2.dat';
-system( ['rm ', dfn] );
-
-rootPath = [getSMuRFenv('SMURF_EPICS_ROOT'),':AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[0]:'];
-takeDebugData(rootPath,dfn,2^16);
-
-[f, df, frs] = decodeSingleChannel(dfn);
-%[f,df,frs]=decodeData(dfn);
+function [f,df,frs]=quickDataSingleChannel(band)
+    % take a short dumb dataset
+    dfn='/tmp/tmp2.dat';
+    system( ['rm ', dfn] );
+    
+    rootPath = [getSMuRFenv('SMURF_EPICS_ROOT'),sprintf(':AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[%d]:',band)];
+    takeDebugData(band,dfn,2^16);
+    
+    [f, df, frs] = decodeSingleChannel(dfn);
+    %[f,df,frs]=decodeData(dfn);
