da4da8bfc2bc621739eb4a66cc4a037865e66cf1
cryo <cryo@testbox03.stanford.edu>
2018-08-29 22:34:25 -0700

    644->755 permissions for tone files

--- software/CryoDetCmbHcd/matlab/readAdcData.m
+++ software/CryoDetCmbHcd/matlab/readAdcData.m
@@ -16,10 +16,14 @@ function [data] = readAdcData( rootPath, adcNumber, varargin )
         dataLength = varargin{1};     
     end
 
-    setupDaqMux( rootPath, 'adc', adcNumber, dataLength );   
+    setupDaqMux( 2, 'adc', adcNumber, dataLength );   
 
     results = readStreamData( rootPath, dataLength); 
     
     data = results(2,:) + 1i.*results(1,:);
+    
+    if nargout == 0
+       figure; pwelch(data, [], [], [], 614.4, 'centered'); 
+    end
 
 end
