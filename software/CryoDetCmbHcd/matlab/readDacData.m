80131e40b33b4c107640312d25abb63b558f40db
cryo <cryo@testbox03.stanford.edu>
2018-08-29 22:40:07 -0700

    Latest versions.

--- software/CryoDetCmbHcd/matlab/readDacData.m
+++ software/CryoDetCmbHcd/matlab/readDacData.m
@@ -16,10 +16,15 @@ function [data] = readDacData( rootPath, dacNumber, varargin )
         dataLength = varargin{1};     
     end
 
-    setupDaqMux( rootPath, 'dac', dacNumber, dataLength );   
+    setupDaqMux( 0, 'dac', dacNumber, dataLength );   
 
     results = readStreamData( rootPath, dataLength); 
     
     data = results(2,:) + 1i.*results(1,:);
+    
+    if nargout == 0
+       figure; pwelch(data, [], [], [], 614.4, 'centered'); 
+    end
+
 
 end
