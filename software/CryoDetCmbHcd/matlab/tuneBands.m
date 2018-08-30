01128bb23ff628bcf3e3b1065f6967edb534ab44
cryo <cryo@testbox03.stanford.edu>
2018-08-29 22:17:21 -0700

    Added subbands list and Adrive dependency to tuneBands.  Also now reLocks each band, runs trackingSetup, and checkLock.  Now prints channels that are tracking and survive checkLock upon complete.

--- software/CryoDetCmbHcd/matlab/tuneBands.m
+++ software/CryoDetCmbHcd/matlab/tuneBands.m
@@ -1,5 +1,7 @@
-function tuneBands(bands)
-    Adrive=9;
+function etas = tuneBands(bands, subbands,Adrive)
+    if nargin < 3
+        Adrive=11;
+    end
     doEtaPlots=true;
     lockBandAtEndOfEtaScan=false;
     
@@ -8,11 +10,17 @@ function tuneBands(bands)
     etas = cell(1,nbands);
     band_counter=1;
     for band=bands
-        resFreqs{band_counter} = findFreqs(band);
+        resFreqs{band_counter} = findFreqs(band, subbands);
         etas{band_counter} = setupNotches_umux16(band,Adrive,doEtaPlots,lockBandAtEndOfEtaScan,resFreqs{band_counter});
         Off(band);
-        reLock(band,etas{band_counter});
-        band_counter=band_counter+1;        
+        reLock(band,etas{band_counter});     
+        
+        chans=whichOn(band);
+        trackingSetup(band,chans(1));
+        checkLock(band,etas{band_counter});
+        
+        band_counter=band_counter+1;   
+        close all;
     end
     
     %% summarize
@@ -22,7 +30,10 @@ function tuneBands(bands)
         disp(sprintf('findFreqs = %s',resFreqs{band_counter}));
         disp(sprintf('etaScan = %s',etas{band_counter}));
         band_counter=band_counter+1;
+        
+        disp(sprintf('length(whichOn(%d))=%d',band,length(whichOn(band))));
     end
+   
     %% running this kills band 3
     %reLock(2,eta2);
     
